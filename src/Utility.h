a1973c24144e8a40be1678f4efae1c84d2a5000a
timdiels <tim@timdiels.be>
2012-12-11 19:11:26 +0100

    Forgot to add this

---
+++ src/Utility.h
@@ -0,0 +1,18 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#pragma once
+
+#include "Point.h"
+
+inline int at(SDL_Point tile_pos) {
+    return tile_pos.y * MAP_WIDTH + tile_pos.x;
+}
+
