2e2bd4c7bfedebc4d7e17288515ddf3c90dd6b67
timdiels <tim@timdiels.be>
2012-12-11 14:11:13 +0100

    Forgot to add these

---
+++ src/Food.h
@@ -0,0 +1,21 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#pragma once
+
+namespace Food {
+    enum Type : unsigned char {
+        NONE,
+        DOT,
+        ENERGIZER
+    };
+}
+typedef Food::Type Foods[MAP_WIDTH * MAP_HEIGHT];
+
