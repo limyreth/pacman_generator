4544144e55b426ccc73ab0d005f8f16fc0b97529
timdiels <tim@timdiels.be>
2013-01-29 11:43:03 +0100

    Fix: tests no longer ran

--- src/pacman/test/Tests.cpp
+++ src/pacman/test/Tests.cpp
@@ -25,18 +25,18 @@ namespace PACMAN {
 
 void test(string name) {
     if (name == "MODEL::test_start_foods") MODEL::test_start_foods();
-    else if (name == "MODEL::test_initial_game_state") MODEL::test_initial_game_state;
-    else if (name == "MODEL::test_ghosts_remain_normal_when_not_eating_energizer") MODEL::test_ghosts_remain_normal_when_not_eating_energizer;
-    else if (name == "MODEL::test_pacman_movement_regular_speed_not_cornering") MODEL::test_pacman_movement_regular_speed_not_cornering;
-    else if (name == "MODEL::test_dot_eating_no_cornering") MODEL::test_dot_eating_no_cornering;
-    else if (name == "MODEL::test_eat_2_dots") MODEL::test_eat_2_dots;
-    else if (name == "MODEL::test_eat_energizer") MODEL::test_eat_energizer;
-    else if (name == "MODEL::test_eat_fruit") MODEL::test_eat_fruit;
-    else if (name == "MODEL::test_eat_fruit_ghost") MODEL::test_eat_fruit_ghost;
-    else if (name == "MinimaxTests::test_1") MinimaxTests::test_1;
-    else if (name == "GeneratorTests::test_1") GeneratorTests::test_1;
-    else if (name == "GeneratorTests::test_save_load") GeneratorTests::test_save_load;
-    else if (name == "GeneratorTests::test_save_load_of_running_instance") GeneratorTests::test_save_load_of_running_instance;
+    else if (name == "MODEL::test_initial_game_state") MODEL::test_initial_game_state();
+    else if (name == "MODEL::test_ghosts_remain_normal_when_not_eating_energizer") MODEL::test_ghosts_remain_normal_when_not_eating_energizer();
+    else if (name == "MODEL::test_pacman_movement_regular_speed_not_cornering") MODEL::test_pacman_movement_regular_speed_not_cornering();
+    else if (name == "MODEL::test_dot_eating_no_cornering") MODEL::test_dot_eating_no_cornering();
+    else if (name == "MODEL::test_eat_2_dots") MODEL::test_eat_2_dots();
+    else if (name == "MODEL::test_eat_energizer") MODEL::test_eat_energizer();
+    else if (name == "MODEL::test_eat_fruit") MODEL::test_eat_fruit();
+    else if (name == "MODEL::test_eat_fruit_ghost") MODEL::test_eat_fruit_ghost();
+    else if (name == "MinimaxTests::test_1") MinimaxTests::test_1();
+    else if (name == "GeneratorTests::test_1") GeneratorTests::test_1();
+    else if (name == "GeneratorTests::test_save_load") GeneratorTests::test_save_load();
+    else if (name == "GeneratorTests::test_save_load_of_running_instance") GeneratorTests::test_save_load_of_running_instance();
     else ASSERT(false);
 }
 
